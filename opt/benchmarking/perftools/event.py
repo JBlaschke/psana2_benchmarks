2eed6eb64e57687b1e345e24c90f081a4a0e736a
Johannes Blaschke <jpblaschke@lbl.gov>
2020-05-16 14:21:38 -0700

    fix timestamping

--- opt/benchmarking/perftools/event.py
+++ opt/benchmarking/perftools/event.py
@@ -12,22 +12,48 @@ from ..util import Singleton
 class Event(object):
 
 
-    def __init__(self):
-        self._t_evt = self._time()
+    def __init__(self, accuracy=1000):
+
+        self._accuracy = accuracy
+        self._t_evt    = self._time(self._accuracy)
 
 
     @staticmethod
-    def _time():
+    def _time(accuracy):
 
         t = time()
         s = int(floor(t))
-        return (s, int(round((t - s) * 1000)))
+        return (s, int(round((t - s) * accuracy)))
+
+
+    @property
+    def accuracy(self):
+        self._accuracy
 
 
     @property
     def timestamp(self):
         
-        s, t = self._t_evt
+        t, s = self._t_evt
         return strftime("%Y-%m-%dT%H:%MZ%S", gmtime(t)) + (".%03d" % s)
 
 
+class EventLogger(object, metaclass=Singleton):
+    
+    def __init__(self):
+        self._events = list()
+
+
+    def add(self, evt):
+        self._events.add(evt)
+
+
+    @property
+    def events(self):
+        return self._events
+
+
+    @property
+    def timestamps(self):
+        for e in self.events:
+            yield e.timestamp
--- opt/test_event.py
+++ opt/test_event.py
@@ -2,8 +2,23 @@
 # -*- coding: utf-8 -*-
 
 
-from benchmarking import Event
+from time import sleep
 
-e1 = Event()
 
-print(e1.timestamp)
+from benchmarking import Event, EventLogger
+
+
+def test_timers(n):
+
+    e1 = Event()
+    sleep(n)  # sleep for n seconds
+    e2 = Event()
+
+    return e1, e2
+
+
+if __name__ == "__main__":
+
+    e1, e2 = test_timers(2)
+    print("Testing event timers:")
+    print(f"e1 = {e1.timestamp}\ne2 = {e2.timestamp}")
