aa0b1ad826db3be1f05f3737a89046283e65ae4b
Johannes Blaschke <jpblaschke@lbl.gov>
2020-05-16 09:57:26 -0700

    project reorganization

---
+++ opt/benchmarking/perftools/event.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import time
+
+
+
+class Event(object):
+
+
+    def __init__(self):
+        self._t_evt = self._time()
+
+
+    @staticmethod
+    def _time():
+
+        t = time.time()
+        s = int(math.floor(t))
+        return (s, int(round((t - s) * 1000)))
+
+
+    @property
+    def timestamp(self):
+        
+        s, t = self._t_evt
+        return time.strftime("%Y-%m-%dT%H:%MZ%S", time.gmtime(t)) + (".%03d" % s)
